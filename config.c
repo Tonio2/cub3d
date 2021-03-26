3ca7aa4bb56a9f26f805e5793b66c794fee60105
Antoine Labalette <alabalet@student.42nice.fr>
2021-03-26 15:06:00 +0100

    [a] parse config file to get map_lines

--- config.c
+++ config.c
@@ -6,7 +6,7 @@
 /*   By: alabalet <alabalet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/25 14:35:32 by alabalet          #+#    #+#             */
-/*   Updated: 2021/03/26 13:25:46 by alabalet         ###   ########.fr       */
+/*   Updated: 2021/03/26 15:04:50 by alabalet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,29 +50,61 @@ void	ft_parse_config(int fd, t_cub *config)
 	line = 0;
 	while (!is_config_ready(config))
 	{
+		if (get_next_line(fd, &line) <= 0)
+			ft_print_error("Erreur lors de la lecture du fichier");
 		if (ft_len(line))
 			ft_parse_config_line(line, config);
-		if (get_next_line(fd, &line) <= 0)
-			ft_print_error("Erreur lors de la lecture du fichier");
 	}
-	while (ft_len(line) == 0)
-		if (get_next_line(fd, &line) <= 0)
-			ft_print_error("Erreur lors de la lecture du fichier");
 }
 
-char	**ft_parse_map(int fd)
+t_map_line	*ft_parse_map(int fd)
 {
-	char	**lines;
+	t_map_line	*first_elt;
+	t_map_line	*last_elt;
+	t_map_line	*cur_elt;
+	char		*line;
+	int			gnl_ret;
 
-	lines = 0;
-	fd++;
-	return (lines);
+	first_elt = malloc(sizeof(t_map_line));
+	first_elt->next = 0;
+	last_elt = first_elt;
+	cur_elt = first_elt;
+	line = 0;
+	while (ft_len(line) == 0)
+		get_next_line(fd, &line);
+	first_elt->line = ft_strcreate(line);
+	gnl_ret = get_next_line(fd, &line);
+	while (gnl_ret == 1 && ft_len(line))
+	{
+		cur_elt = malloc(sizeof(t_map_line));
+		last_elt->next = cur_elt;
+		cur_elt->line = ft_strcreate(line);
+		cur_elt->next = 0;
+		last_elt = cur_elt;
+		gnl_ret = get_next_line(fd, &line);
+	}
+	while (gnl_ret)
+	{
+		gnl_ret = get_next_line(fd, &line);
+		if (ft_len(line))
+			ft_print_error("Non empty line after map");
+	}
+	return (first_elt);
+}
+
+void	ft_print_map_lines(t_map_line *first_elt)
+{
+	while (first_elt)
+	{
+		printf("%s\n", first_elt->line);
+		first_elt = first_elt->next;
+	}
 }
 
 void	ft_parse_config_file(char *filename, t_cub *config)
 {
-	int		fd;
-	char	**map_lines;
+	int			fd;
+	t_map_line	*map_lines;
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
@@ -80,5 +112,6 @@ void	ft_parse_config_file(char *filename, t_cub *config)
 	ft_config_init(config);
 	ft_parse_config(fd, config);
 	map_lines = ft_parse_map(fd);
-	ft_parse_map_lines(map_lines, config);
+	ft_print_map_lines(map_lines);
+	//ft_parse_map_lines(map_lines, config);
 }
--- constant.h
+++ constant.h
@@ -6,7 +6,7 @@
 /*   By: alabalet <alabalet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/25 14:27:13 by alabalet          #+#    #+#             */
-/*   Updated: 2021/03/26 13:24:27 by alabalet         ###   ########.fr       */
+/*   Updated: 2021/03/26 14:56:38 by alabalet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,27 +33,33 @@ typedef struct s_cub
 	char	pos_init;
 }			t_cub;
 
-char	*ft_strcreate(char *str);
-int		ft_in(char c, char *str);
+typedef struct s_map_line
+{
+	char				*line;
+	struct s_map_line	*next;
+}						t_map_line;
 
-int		ft_len(char *str);
-void	ft_strcpy(char *dst, char *src);
-void	ft_strcat(char **dst, char *src);
-int		ft_parse_buf(char **dst, char *src);
-int		get_next_line(int fd, char **line);
+char		*ft_strcreate(char *str);
+int			ft_in(char c, char *str);
 
-void	ft_print_error(char *msg);
+int			ft_len(char *str);
+void		ft_strcpy(char *dst, char *src);
+void		ft_strcat(char **dst, char *src);
+int			ft_parse_buf(char **dst, char *src);
+int			get_next_line(int fd, char **line);
 
-void	ft_parse_dim(char *str, t_cub *config);
-int		check_texture_path(char *str);
-void	ft_parse_texture(char *str, t_cub *config, int mode);
-void	ft_parse_color(char *str, t_cub *config, int mode);
-void	ft_parse_config_line(char *str, t_cub *config);
+void		ft_print_error(char *msg);
 
-void	ft_parse_map_lines(char **lines, t_cub *config);
-char	**ft_parse_map(int fd);
+void		ft_parse_dim(char *str, t_cub *config);
+int			check_texture_path(char *str);
+void		ft_parse_texture(char *str, t_cub *config, int mode);
+void		ft_parse_color(char *str, t_cub *config, int mode);
+void		ft_parse_config_line(char *str, t_cub *config);
 
-void	ft_config_init(t_cub *config);
-int		is_config_ready(t_cub *config);
-void	ft_parse_config_file(char *filename, t_cub *config);
+void		ft_parse_map_lines(char **lines, t_cub *config);
+t_map_line	*ft_parse_map(int fd);
+
+void		ft_config_init(t_cub *config);
+int			is_config_ready(t_cub *config);
+void		ft_parse_config_file(char *filename, t_cub *config);
 #endif
